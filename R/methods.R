b581b9f43ce684eed3f68e471a318eaa3fadb907
‘topepo’ <mxkuhn@gmail.com>
2025-01-14 20:09:18 -0500

    use cli instead of cat()

--- R/methods.R
+++ R/methods.R
@@ -1,15 +1,25 @@
 #' @export
 print.nn_adjust <- function(x, ...) {
-  cat("Post-hoc Nearest Neighbor Adjustments\n")
-  cls <- x$fit %>% hardhat::extract_spec_parsnip() %>% class() %>% purrr::pluck(1)
-  cat("Model:", cls, "\n")
-  cat("Training set: n = ", length(x$predictions), ", ", ncol(x$predictors), " predictors\n", sep = "")
-  invisible(x)
+	cls <- x$fit %>% hardhat::extract_spec_parsnip() %>% class()
+	cli::cli_inform("Post-hoc Nearest Neighbor Adjustments\n")
+	cli::cli_inform("Model classes: {cls}")
+	cli::cli_inform("Reference data:")
+	cli::cli_inform(c(i = "{length(x$outcome)} sample{?s}"))
+	cli::cli_inform(c(i = "{ncol(x$predictors)} predictor{?s}"))
+	invisible(x)
+}
+
+print.nn_cal_adjust <- function(x, ...) {
+	cli::cli_inform("Post-hoc Nearest Neighbor Adjustments\n")
+	cli::cli_inform("Reference data:")
+	cli::cli_inform(c(i = "{length(x$outcome)} sample{?s}"))
+	cli::cli_inform(c(i = "{ncol(x$predictors)} predictor{?s}"))
+	invisible(x)
 }
 
 #' @export
 required_pkgs.nn_adjust <- function(x, ...) {
-  c("adjusted", x$pkgs)
+	c("adjusted", x$pkgs)
 }
 
 # Add multi_predict
